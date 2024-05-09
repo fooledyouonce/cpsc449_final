98cb29a5661a32e493a11368f6415d0a86d8ed22
Mike Thai <mikethai126@gmail.com>
2024-05-09 02:57:40 -0700

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Tasks (
-    task_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(100) NOT NULL,
-    description TEXT,
-    due_date DATE,
-    completed BOOLEAN DEFAULT FALSE
-);
